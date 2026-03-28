3b1a76ea131a669e4ec2bc056e70a777d15656ad
clafish <andrew.korzh2008@gmail.com>
2026-03-28 14:29:04 +0200

    second part of task

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace _123.week8_clafish.Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> container = new();
+
+    public void AddItem(T item)
+    {
+        container.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double sum = 0;
+        foreach (var delivery in container)
+        {
+            sum += delivery.CalculateCost();
+        }
+
+        return sum;
+    }
+}
