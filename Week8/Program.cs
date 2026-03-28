eba4d613697c8a0851d26be356c6ff28fa394a3c
clafish <andrew.korzh2008@gmail.com>
2026-03-28 14:29:29 +0200

    last part of task

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace _123.week8_clafish.Week8;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        var letter1 = new Letter("A1", 1);
+        var letter2 = new Letter("A2", 2);
+
+        var parcel1 = new Parcel("B1", 69, "175x900x15");
+        var parcel2 = new Parcel("B2", 80, "155x800x15");
+        
+        letter1.PrintInfo();
+        Console.WriteLine();
+        parcel2.PrintInfo();
+
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+        
+        myCargo.AddItem(letter1);
+        myCargo.AddItem(letter2);
+        myCargo.AddItem(parcel1);
+        myCargo.AddItem(parcel2);
+
+        Console.WriteLine($"Total cost will be {myCargo.GetTotalCost()}");
+    }
+}
